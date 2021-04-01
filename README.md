8cca0c2b2dcf2eb2b4372d314f4e2fcaf50e7afd
Maksim Kostromin <daggerok@gmail.com>
2021-04-01 15:49:42 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# wipe4j
+IN PROGRESS: JVM projects cleaner analyzer
---
+++ wipe4j-core/src/test/java/com/github/daggeerok/wipe4j/core/CoreApplicationTests.java
@@ -0,0 +1,11 @@
+package com.github.daggeerok.wipe4j.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreApplicationTests {
+
+  @Test
+  void contextLoads() { }
+}
